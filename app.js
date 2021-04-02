cb7edb56f4effc7de54b7c09dfdb82762f58d8b0
Hector <ctstylez@hotmail.com>
2021-04-02 18:53:10 -0400

    Co-authored-by: Orlando <0ly@users.noreply.github.com>

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ function fetchData(url) {
   return fetch(url)
     .then(checkStatus)
     .then(res => res.json())
-    .catch(error => console.log('Looks llike there was a problem!', error));
+    .catch(error => console.log('Looks like there was a problem!', error));
 }
 
-  fetchData('https://randomuser.me/api/?page=1&results=12&seed=employee')
+fetchData('https://randomuser.me/api/?results=12&seed=employee')
   .then(data => {
-    const employees = data.results;
-    generateCards(employees)
-    generateModal(employees)
+    let dataList = data.results
+    generateCards(dataList)
+    browseModal(dataList)
   })
 
 function checkStatus(response) {
@@ -29,56 +29,107 @@ function checkStatus(response) {
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
-function generateCards(employees) {
+function generateCards(dataList) {
   const galleryDiv = document.getElementById('gallery');
   galleryDiv.innerHTML = '';
-    employees.forEach(card => {
-      const cardHtml =
-          `<div class="card">
-              <div class="card-img-container">
-                  <img class="card-img" src= ${card.picture.large} alt="profile picture">
-              </div>
-              <div class="card-list-container">
-                  <h3 id="name" class="card-name cap"> ${card.name.first} </h3>
-                  <p class="card-text"> ${card.email} </p>
-                  <p class="card-text cap"> ${card.location.city}, ${card.location.state} </p>
-              </div>
-          </div> `;
-
-        galleryDiv.insertAdjacentHTML("beforeend", cardHtml);
-    })
+  dataList.forEach(employee => {
+    const cardHtml =
+      `<div class="card" id="${employee.email}">
+                <div class="card-img-container">
+                    <img class="card-img" src= ${employee.picture.large} alt="profile picture">
+                </div>
+                <div class="card-list-container">
+                    <h3 id="name" class="card-name cap"> ${employee.name.first} ${employee.name.last} </h3>
+                    <p class="card-text"> ${employee.email} </p>
+                    <p class="card-text cap"> ${employee.location.city}, ${employee.location.state}</p>
+                </div>
+            </div> `;
+    galleryDiv.insertAdjacentHTML("beforeend", cardHtml);
+  })
 }
 
-function generateModal(employees){
-  const cardDiv = document.querySelectorAll('.card');
-  
-  cardDiv.forEach( item => {
-  item.addEventListener( "click", () => {
+function generateModal() {
+  const modalDiv = document.createElement("div");
+  modalDiv.className = 'modal-container';
+  document.body.appendChild(modalDiv);
 
-    const modalDiv = document.createElement("div");
-    modalDiv.className = 'modal-container';
-    
-    const modalHtml =
-      ` <div class="modal">
+  const modalHtml =
+    ` <div class="modal">
           <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-                <img class="modal-img" src= "https://placehold.it/125x125" alt="profile picture">
-                <h3 id="name" class="modal-name cap">name</h3>
-                <p class="modal-text">email</p>
-                <p class="modal-text cap">city</p>
-                <hr>
-                <p class="modal-text">(555) 555-5555</p>
-                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                <p class="modal-text">Birthday: 10/21/2015</p>
+            
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
             </div>
         </div>`;
-    document.body.appendChild(modalDiv).insertAdjacentHTML('afterbegin', modalHtml);
 
-    const modalCloseBtn = document.getElementById('modal-close-btn');
-    modalCloseBtn.addEventListener('click', () => {
-      modalDiv.remove();
+  modalDiv.insertAdjacentHTML('afterbegin', modalHtml);
+  // Trigger close modal
+  const modalCloseBtn = document.getElementById('modal-close-btn');
+  modalCloseBtn.addEventListener('click', () => { modalDiv.remove() })
+}
+
+function browseModal(data, index) {
+  const cardDiv = document.querySelectorAll('.card');
+  for (let i = 0; i < cardDiv.length; i++) {
+    cardDiv[i].addEventListener('click', () => {
+      index = data.indexOf(data[i]);
+      generateModal()
+      const prevButton = document.querySelector('#modal-prev');
+      const nextButton = document.querySelector('#modal-next');
+      modalInfoUpdate(data[i])
+
+      if (index < 0) {
+        nextButton.disabled = true;
+      }
+
+      [nextButton, prevButton].forEach(button => {
+        button.addEventListener('click', (e) => {
+          const clickedButton = e.target;
+          const modalContainer = document.querySelector('.modal-info-container');
+
+          if (clickedButton.id == 'modal-next') {
+            index++;
+            if (index <= data.length) {
+              modalContainer.remove();
+              modalInfoUpdate(data[index]);
+            }
+
+            if (index === data.length - 1) {
+              nextButton.disabled = true;
+            } else if (index < data.length) {
+              prevButton.disabled = false;
+            };
+          }
+
+          else if (clickedButton.id == 'modal-prev') {
+            index--;
+            if (index <= -1) {
+              index = -1;
+            } else if (index <= data.length) {
+              modalContainer.remove();
+              modalInfoUpdate(data[index]);
+              nextButton.disabled = false;
+            };
+          }
+        })
+      })
     })
-  })
-})
+  }
+}
 
+function modalInfoUpdate(employee) {
+  let modalContainer = document.querySelector('.modal-btn-container');
+  let modalEmployeeInfo =
+    `<div class="modal-info-container">
+              <img class="modal-img" src= ${employee.picture.large} alt="profile picture">
+              <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+              <p class="modal-text">${employee.email}</p>
+              <p class="modal-text cap">${employee.location.city}</p>
+              <hr>
+              <p class="modal-text">${employee.phone}</p>
+              <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, OR ${employee.location.postcode}</p>
+              <p class="modal-text">Birthday: ${employee.dob.date}</p>
+            </div>`;
+  modalContainer.insertAdjacentHTML('beforebegin', modalEmployeeInfo);
 }
